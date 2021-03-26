514f289af2f5e60be63b14fcccd00d66575db26d
Nguyen Viet Hung <nguyen.viet.hung@sun-asterisk.com>
2021-03-26 17:41:54 +0700

    add docker-compose files

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sleep 15 # TODO: use nc or wait-for-it later
+
+rm -f /app/tmp/pids/server.pid
+bundle exec rake db:create db:migrate
+
+bundle exec rails server -b 0.0.0.0 -p 3000
